d32134a8bb554f9f7a16426d77558a8d5fd67003
Florian Estie <florianestie@gmail.com>
2018-10-09 11:55:57 +0200

    Files Relocated

---
+++ Business Website /Script/JF.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"> 
+</script>
+<link rel = 'stylesheet'href = 'Coffeesite.CSS'>
+</link>
+<title> Coffee </title>
+</head>
+
+<body>
+<nav>
+   <ul>
+     <li class="sub-menu-parent" tab-index="0">
+       <a href="Start"><b>COFFEE</b></a>
+       <ul class="sub-menu">
+         <li><a href="#">ABOUT US</a></li>
+         <li><a href="#">TEAM</a></li>
+        </ul>
+     
+     </li>
+     <li class="sub-menu-parent" tab-index="0">
+       <a href="Middle"> <b>PRODUCTS</b></a>
+       <ul class="sub-menu">
+         <li><a href="Our_Products.html">SIGHT GLASS</a></li>
+         <li><a href="#">ATOMIC</a></li>
+         <li><a href="#">ESTATE COFFEE</a></li>
+       </ul>
+     </li>
+     
+     <li class="sub-menu-parent" tab-index="0">
+       <a href="End"><b>CONTACT</b></a>
+       <ul class="sub-menu">
+         <li><a href="#">EMAIL</a></li>
+         <li><a href="#">LOCATION</a></li>
+       </ul></li>
+   </ul>
+ </nav>
+
+ <div class="parallaxJames"></div>
+
+
+</body>	
+	
+</html>
